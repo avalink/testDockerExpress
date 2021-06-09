e5be9cac16b17f5ce30fd6d60bc11367f22ddfcd
avalink <ruru6615@gmail.com>
2021-06-10 01:37:59 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,16 @@
-const express = require('express')();
-const server = require('http').createServer(express);
-server.listen (3000, () => {
-  console.log ('SERVER STARTED');
-  console.log ('port:', 3000);
+'use strict';
+
+const express = require('express');
+
+// Constants
+const PORT = 8080;
+const HOST = '0.0.0.0';
+
+// App
+const app = express();
+app.get('/', (req, res) => {
+  res.send('Hello World');
 });
+
+app.listen(PORT, HOST);
+console.log(`Running on http://${HOST}:${PORT}`);
